966b04dab11f809881594407b98b91053c48db1f
JohnsonOdang <johnnyodanny@gmail.com>
2024-03-07 18:56:19 -0800

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
+import About from './components/About'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Home />
+      <About />
     </div>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import AboutBackground from "../assets/about-background.png";
+import AboutBackgroundImage from "../assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
 
 function About() {
   return (
-    <>
-      <h1>About Section</h1>
-    </>
+    <div className='about-section-container'>
+      <div className='about-background-image-container'>
+        <img src={AboutBackground} />
+      </div>
+      <div className='about-section-image-container'>
+        <img src={AboutBackgroundImage} alt='' />
+      </div>
+      <div className='about-section-text-container'>
+        <p className='primary-subheading'>About</p>
+        <h1 className='primary-heading'>
+          Food Is An Important Part Of a Balanced Diet
+        </h1>
+        <p className='primary-text'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim est officia harum ipsum, quos repudiandae, expedita nobis architecto quibusdam excepturi ab sed voluptatum voluptate sunt.
+        </p>
+        <p className='primary-text'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit placeat reprehenderit nihil deserunt nam voluptatibus porro voluptate facilis! Molestias consequatur excepturi cum cumque animi magni inventore maxime harum similique ipsam.
+        </p>
+        <div className='about-buttons-container'>
+          <button className='secondary-button'>Learn More</button>
+          <button className='watch-video-button'>
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
